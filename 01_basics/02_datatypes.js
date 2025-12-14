708cb021bab9d11db616427cbb4ecd9845e503ba
hamzaansari00567-sudo <hamzaansari00567@gmail.com>
2025-12-14 20:47:56 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"//treat all JS  code as a new version
+
+//alert(3+3)// we are using NODEJS,not a browser
+
+let name="hamza" // string datatype
+let age=18//number
+let isloggedIn = false
+let state= null
+
+// number=>2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null =>standlone value
+//undefined =>
+//symbol => unique
+
+console.log(typeof"hamza")
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object

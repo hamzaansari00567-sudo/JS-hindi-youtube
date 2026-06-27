231ceafc4d8f98c95c2005bd57a178ce89cdb387
hamzaansari00567-sudo <hamzaansari00567@gmail.com>
2026-06-27 21:49:44 +0000

    converionandoperation

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -20,3 +20,23 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 let someNumber = 33
 let stringNumber=String(someNumber);
 console.log(typeof stringNumber)
+/****************operation***************/
+let value = 3
+let negValue=+value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = "  hamza"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+
+console.log(+true);
+console.log(+"");
+
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);

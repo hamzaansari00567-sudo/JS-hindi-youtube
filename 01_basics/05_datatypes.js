effe35b260acb53b1549caace9cc875324281a4b
hamzaansari00567-sudo <hamzaansari00567@gmail.com>
2026-06-29 11:39:09 +0000

    comparision and datatype

---
+++ 01_basics/05_datatypes.js
@@ -0,0 +1,31 @@
+//primitive datatype
+// 7 type:- string ,number, boolean, null,undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.5
+const isLoggediIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId =Symbol('123')
+
+//console.log( id == anotherId);
+
+// const bignumber = 23456789456n
+//non primitive  type (Reference type)
+// like array ,objects, function
+
+const heros = ["spiderman","ironman","batman"];
+
+let myOBJ={
+    name = "hamza" ,
+    age = 15,
+
+}
+
+const myFuntion = function(){
+    console.log("helo, world");
+    
+}
+console.log(typeof anotherId);
